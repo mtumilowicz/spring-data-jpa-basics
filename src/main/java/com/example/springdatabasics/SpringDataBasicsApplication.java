6469fd56f7392d8d98927c782430175000d72d6f
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-02 23:05:47 +0200

    Initial commit

---
+++ src/main/java/com/example/springdatabasics/SpringDataBasicsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdatabasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataBasicsApplication.class, args);
+    }
+}
