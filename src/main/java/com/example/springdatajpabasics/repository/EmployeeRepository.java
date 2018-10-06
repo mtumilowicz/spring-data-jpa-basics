2068b948320e94540a9bd1af4b160cb52a197e81
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-06 10:40:07 +0200

    repository methods + tests

--- src/main/java/com/example/springdatajpabasics/repository/EmployeeRepository.java
+++ src/main/java/com/example/springdatajpabasics/repository/EmployeeRepository.java
@@ -2,9 +2,18 @@ package com.example.springdatajpabasics.repository;
 
 import com.example.springdatajpabasics.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by mtumilowicz on 2018-10-03.
  */
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findByName(String name);
+    Optional<Employee> findTop1ByName(String name);
+    List<Employee> findByIssue_Description(String description);
+    List<Employee> findDistinctEmployeByNameIgnoreCase(String name);
 }
--- src/test/java/com/example/springdatajpabasics/SpringDataBasicsTest.java
+++ src/test/java/com/example/springdatajpabasics/SpringDataBasicsTest.java
@@ -1,7 +1,47 @@
 package com.example.springdatajpabasics;
 
+import com.example.springdatajpabasics.entity.Employee;
+import com.example.springdatajpabasics.repository.EmployeeRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by mtumilowicz on 2018-10-05.
  */
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class SpringDataBasicsTest {
+    
+    @Autowired
+    EmployeeRepository repository;
+    
+    
+    @Test
+    public void paging() {
+        Page<Employee> all = repository.findAll(PageRequest.of(2, 2));
+        
+        assertThat(all.getTotalElements(), is(4));
+        assertThat(all.getTotalPages(), is(2));
+    }
+
+    @Test
+    public void sorting() {
+        List<Employee> employees = repository.findAll(Sort.by("name").ascending());
+    }
+
+    @Test
+    public void pagingAndSorting() {
+        Page<Employee> name = repository.findAll(PageRequest.of(2, 2, Sort.by("name").ascending()));
+    }
 }
