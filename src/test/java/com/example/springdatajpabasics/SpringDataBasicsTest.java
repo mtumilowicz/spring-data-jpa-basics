14f4fb40f3406e47182e04011b7d5bee91312999
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-06 12:53:26 +0200

    tests

--- src/test/java/com/example/springdatajpabasics/SpringDataBasicsTest.java
+++ src/test/java/com/example/springdatajpabasics/SpringDataBasicsTest.java
@@ -12,6 +12,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
 
 /**
  * Created by mtumilowicz on 2018-10-05.
@@ -19,51 +25,86 @@ import java.util.List;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class SpringDataBasicsTest {
-    
+
     @Autowired
     EmployeeRepository repository;
-    
-    
+
+
     @Test
     public void paging() {
-        Page<Employee> all = repository.findAll(PageRequest.of(2, 2));
-        
-//        assertThat(all.getTotalElements(), is(4));
-//        assertThat(all.getTotalPages(), is(2));
+        Page<Employee> pages = repository.findAll(PageRequest.of(2, 2));
+
+        assertThat(pages.getTotalElements(), is(4L));
+        assertThat(pages.getTotalPages(), is(2));
     }
 
     @Test
     public void sorting() {
         List<Employee> employees = repository.findAll(Sort.by("name").ascending());
+
+        assertThat(employees, hasSize(4));
+
+        Employee employee1 = employees.get(0);
+        Employee employee2 = employees.get(1);
+        Employee employee3 = employees.get(2);
+        Employee employee4 = employees.get(3);
+
+        assertThat(employee1.getName(), is("Hemingway"));
+        assertThat(employee2.getName(), is("Lem"));
+        assertThat(employee3.getName(), is("Mrozek"));
+        assertThat(employee4.getName(), is("Tumilowicz"));
+
     }
 
     @Test
     public void pagingAndSorting() {
-        Page<Employee> name = repository.findAll(PageRequest.of(2, 2, Sort.by("name").ascending()));
+        PageRequest pageRequest = PageRequest.of(0, 2, Sort.by("address.city").ascending());
+        Page<Employee> employee = repository.findAll(pageRequest);
+
+
+        assertThat(employee.getContent().get(0).getAddress().getCity(), is("Key West"));
     }
-    
+
     @Test
     public void findByName() {
-        
+        List<Employee> hemingways = repository.findByName("Hemingway");
+
+        assertThat(hemingways, hasSize(1));
+        assertThat(hemingways.get(0).getName(), is("Hemingway"));
     }
 
     @Test
     public void findTop1ByName() {
+        List<Employee> hemingways = repository.findByName("Hemingway");
 
+        assertThat(hemingways, hasSize(1));
+        assertThat(hemingways.get(0).getName(), is("Hemingway"));
     }
-    
+
     @Test
     public void findByIssues_Description() {
-        
+        List<Employee> employees = repository.findByIssues_Description("improve coding skills");
+
+        assertThat(employees, hasSize(1));
+        assertThat(employees.get(0).getName(), is("Tumilowicz"));
     }
-    
+
     @Test
     public void findByAddress_City() {
-        
+        List<String> employeeNames = repository.findByAddress_City("Krakow")
+                .stream()
+                .map(Employee::getName)
+                .collect(Collectors.toList());
+
+        assertThat(employeeNames, hasSize(2));
+        assertThat(employeeNames, contains("Mrozek", "Lem"));
     }
-    
+
     @Test
     public void findDistinctEmployeeByNameIgnoreCase() {
-        
+        List<Employee> employees = repository.findDistinctEmployeeByNameIgnoreCase("MROZEK");
+
+        assertThat(employees, hasSize(1));
+        assertThat(employees.get(0).getName(), is("Mrozek"));
     }
 }
